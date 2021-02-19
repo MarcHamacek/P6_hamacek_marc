97ed6a58828bda98fb8d8decd7364213fa1019f7
MarcHamacek <62935905+MarcHamacek@users.noreply.github.com>
2021-02-19 22:11:16 +0100

    Packages xxs-clean et express-rate-limit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 require('dotenv').config();
+
 
 // Configure et autorise les accès extérieurs
 app.use((req, res, next) => {
@@ -22,6 +25,13 @@ mongoose.connect(process.env.DB_URL,
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//Ajout de la limite de tentative de connexion
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50 // Limite de 50 connexions par addresse IP
+});
+
 // Ajout des routes d'identification et d'authentification
 const userRoutes = require('./routes/user');
 
@@ -32,10 +42,13 @@ const saucesRoutes = require('./routes/sauces');
 // Enregistrement des routeurs, activation d'helmet et de bodyparser
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(limiter);
+app.use(xss());
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.listen(8080);
 
 // Exporte l'application
 module.exports = app;
